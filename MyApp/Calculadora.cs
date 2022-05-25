05dc386eb498c45f08470c659b4fe748163a83ea
CoronelExequiel <execoronel14f@gmail.com>
2022-05-24 21:02:23 -0300

    punto 1 finalizado

---
+++ MyApp/Calculadora.cs
@@ -0,0 +1,36 @@
+﻿namespace Ejercicio1
+{
+    public class Calculadora
+    {
+        //campos
+        public double resultado;
+        //metodos
+        public void Iniciar(double valorInicial)
+        {
+            resultado=valorInicial;
+        }
+        public void Sumar(double valor)
+        {
+            resultado+=valor;
+        }
+        public void Restar(double valor)
+        {
+            resultado-=valor;
+        }
+        public void Multiplicar(double valor)
+        {
+            resultado*=valor;
+        }
+        public void Dividir(double valor)
+        {
+            resultado/=valor;
+        }
+        public void Limpiar()
+        {
+            resultado=0;
+        }
+    }
+}
+
+   
+
